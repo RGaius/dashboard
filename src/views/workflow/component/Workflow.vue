2f337cd6324b698cd6909d18cd413b39b7c4ca09
zhaobo <15528330581@163.com>
2024-07-02 22:15:49 +0800

    feature: 开始工作流布局

--- src/views/workflow/component/Workflow.vue
+++ src/views/workflow/component/Workflow.vue
@@ -1,18 +1,82 @@
-<template>
-  <div ref="graphContainer"></div>
-</template>
-
 <script setup lang="ts">
-  import { onMounted, ref } from 'vue';
-  import { Graph, Platform } from '@antv/x6';
+  import { onMounted, ref, unref } from 'vue';
+  import { Edge, Graph, Path, Platform } from '@antv/x6';
+  import './registerCells';
+  import cells from './cells.json';
+
+  // 引入插件
   import { Selection } from '@antv/x6-plugin-selection';
-  import { register } from '@antv/x6-vue-shape';
 
-  const graphContainer = ref(null);
+  const graph = ref<Graph>();
 
-  onMounted(() => {
+  // 元素校验状态
+  enum CellStatus {
+    DEFAULT = 'default',
+    SUCCESS = 'success',
+    ERROR = 'error',
+  }
+
+  // 节点状态列表
+  const nodeStatusList = [
+    {
+      id: 'node-0',
+      status: 'success',
+    },
+    {
+      id: 'node-1',
+      status: 'success',
+    },
+    {
+      id: 'node-2',
+      status: 'success',
+    },
+    {
+      id: 'node-3',
+      status: 'success',
+    },
+    {
+      id: 'node-4',
+      status: 'error',
+      statusMsg: '错误信息示例',
+    },
+  ];
+
+  // 边状态列表
+  const edgeStatusList = [
+    {
+      id: 'edge-0',
+      status: 'success',
+    },
+    {
+      id: 'edge-1',
+      status: 'success',
+    },
+    {
+      id: 'edge-2',
+      status: 'success',
+    },
+    {
+      id: 'edge-3',
+      status: 'success',
+    },
+  ];
+
+  onMounted(async () => {
+    graph.value = initGraph();
+
+    unref(graph).fromJSON(cells);
+
+    unref(graph).zoomToFit({ padding: { left: 10, right: 10 } });
+
+    excuteAnimate();
+    showNodeStatus();
+    stopAnimate();
+  });
+
+  function initGraph() {
+    const container = document.getElementById('container')!;
     const graph = new Graph({
-      container: graphContainer.value as HTMLElement,
+      container,
       panning: {
         enabled: true,
         eventTypes: ['leftMouseDown', 'mouseWheel'],
@@ -71,12 +135,148 @@
             return false;
           }
           // 只能连接到输入链接桩
-          if (!targetMagnet || targetMagnet.getAttribute('port-group') !== 'in') {
-            return false;
-          }
-          return true;
+          return !(!targetMagnet || targetMagnet.getAttribute('port-group') !== 'in');
         },
       },
     });
-  });
+    graph.use(
+      new Selection({
+        multiple: true,
+        rubberEdge: true,
+        rubberNode: true,
+        modifiers: 'shift',
+        rubberband: true,
+      }),
+    );
+    // 注册连线
+    Graph.registerConnector(
+      'curveConnector',
+      (sourcePoint, targetPoint) => {
+        const hgap = Math.abs(targetPoint.x - sourcePoint.x);
+        const path = new Path();
+        path.appendSegment(Path.createSegment('M', sourcePoint.x - 4, sourcePoint.y));
+        path.appendSegment(Path.createSegment('L', sourcePoint.x + 12, sourcePoint.y));
+        // 水平三阶贝塞尔曲线
+        path.appendSegment(
+          Path.createSegment(
+            'C',
+            sourcePoint.x < targetPoint.x ? sourcePoint.x + hgap / 2 : sourcePoint.x - hgap / 2,
+            sourcePoint.y,
+            sourcePoint.x < targetPoint.x ? targetPoint.x - hgap / 2 : targetPoint.x + hgap / 2,
+            targetPoint.y,
+            targetPoint.x - 6,
+            targetPoint.y,
+          ),
+        );
+        path.appendSegment(Path.createSegment('L', targetPoint.x + 2, targetPoint.y));
+
+        return path.serialize();
+      },
+      true,
+    );
+
+    Edge.config({
+      markup: [
+        {
+          tagName: 'path',
+          selector: 'wrap',
+          attrs: {
+            fill: 'none',
+            cursor: 'pointer',
+            stroke: 'transparent',
+            strokeLinecap: 'round',
+          },
+        },
+        {
+          tagName: 'path',
+          selector: 'line',
+          attrs: {
+            fill: 'none',
+            pointerEvents: 'none',
+          },
+        },
+      ],
+      connector: { name: 'curveConnector' },
+      attrs: {
+        wrap: {
+          connection: true,
+          strokeWidth: 10,
+          strokeLinejoin: 'round',
+        },
+        line: {
+          connection: true,
+          stroke: '#A2B1C3',
+          strokeWidth: 1,
+          targetMarker: {
+            name: 'classic',
+            size: 6,
+          },
+        },
+      },
+    });
+
+    Graph.registerEdge('data-processing-curve', Edge, true);
+    return graph;
+  }
+
+  // 开启边的运行动画
+  const excuteAnimate = () => {
+    unref(graph)
+      .getEdges()
+      .forEach((edge) => {
+        edge.attr({
+          line: {
+            stroke: '#3471F9',
+          },
+        });
+        edge.attr('line/strokeDasharray', 5);
+        edge.attr('line/style/animation', 'running-line 30s infinite linear');
+      });
+  };
+
+  // 关闭边的动画
+  const stopAnimate = () => {
+    unref(graph)
+      .getEdges()
+      .forEach((edge) => {
+        edge.attr('line/strokeDasharray', 0);
+        edge.attr('line/style/animation', '');
+      });
+    edgeStatusList.forEach((item) => {
+      const { id, status } = item;
+      const edge = unref(graph).getCellById(id);
+      if (status === 'success') {
+        edge.attr('line/stroke', '#52c41a');
+      }
+      if (status === 'error') {
+        edge.attr('line/stroke', '#ff4d4f');
+      }
+    });
+    // 默认选中一个节点
+    unref(graph).select('node-2');
+  };
+
+  // 显示节点状态
+  const showNodeStatus = () => {
+    // 优化数据存在性验证
+    nodeStatusList.forEach((item) => {
+      const { id, status, statusMsg } = item;
+      const node = unref(graph)?.getCellById(id);
+      if (node) {
+        const data = node.getData() as CellStatus;
+        console.log(data);
+        node.setData({
+          ...data,
+          status,
+          statusMsg,
+        });
+      }
+    });
+  };
 </script>
+
+<template>
+  <div id="container" style="min-width: 400px; min-height: 600px"></div>
+</template>
+
+<style lang="css" scoped></style>
